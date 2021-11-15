8d578131ae45d3eade765db7f3315fa1637d9040
JacoboPetrelli <92336672+JacoboPetrelli@users.noreply.github.com>
2021-11-15 18:41:42 -0500

    Add files via upload

---
+++ modificarPagina.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Ingrese su nombre y apellido")
+var edad = prompt("Ingrese su edad")
+document.getElementById(saludo).InnerHTML = "Hola ${nombre}"
+let h1saludo = document.querySelector(`#saludo`)
